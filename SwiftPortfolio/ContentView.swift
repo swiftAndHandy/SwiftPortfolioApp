8bb285398b6483a31614e048315b29bba5d0f039
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-03-16 10:35:17 +0100

    Initial Commit

---
+++ SwiftPortfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPortfolio
+//
+//  Created by Andre Veltens on 16.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPortfolio/SwiftPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPortfolioApp.swift
+//  SwiftPortfolio
+//
+//  Created by Andre Veltens on 16.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
