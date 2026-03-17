2b1779e42f2af4d823d06b4830c9fdff00447d0b
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-03-17 14:54:17 +0100

    feat: IssueRowView draft  implemented

--- SwiftPortfolio/ContentView.swift
+++ SwiftPortfolio/ContentView.swift
@@ -9,12 +9,34 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(AppState.self) var appState
+    @Query var allIssues: [Issue]
+    
+    var issues: [Issue] {
+        let filter = appState.selectedFilter ?? .all
+        
+        if let tag = filter.tag {
+            return allIssues.filter { issue in
+                issue.tags?.contains(tag) ?? false
+            }
+        } else {
+            return allIssues.filter { issue in
+                issue.modificationDate > filter.minModificationDate
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Content")
+        List {
+            ForEach(issues) { issue in
+                Text(issue.title)
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
         .modelContainer(.sampleData)
+        .environment(AppState())
 }
---
+++ SwiftPortfolio/Issues/IssueRowView.swift
@@ -0,0 +1,55 @@
+//
+//  IssueRowView.swift
+//  SwiftPortfolio
+//
+//  Created by Andre Veltens on 17.03.26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct IssueRowView: View {
+    @Environment(AppState.self) private var appState
+    var issue: Issue
+    
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1: 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.title)
+                        .font(.headline)
+                        .lineLimit(1)
+                    
+                    Text("No tags")
+                        .foregroundStyle(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.creationDate.formatted(date: .numeric, time: .omitted))
+                        .font(.subheadline)
+                    
+                    if issue.completed {
+                        Text("CLOSED")
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    let container = ModelContainer.sampleData
+    let issue = try! container.mainContext.fetch(FetchDescriptor<Issue>()).first!
+    IssueRowView(issue: issue)
+        .modelContainer(container)
+        .environment(AppState())
+}
